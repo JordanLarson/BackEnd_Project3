7f8f5c6196f34f872d19b2c5ef89c760c2279aea
JordanLarson <jdlarson1234@gmail.com>
2020-07-17 12:59:38 -0700

    added basic routes

---
+++ controllers/usersController.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/users");
+
+router.get("/", (req, res) => res.send("This is root, yeww"));
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.json({ users });
+  } catch (error) {
+    console.log("error");
+    return res.status(500).send(error.message);
+  }
+});
