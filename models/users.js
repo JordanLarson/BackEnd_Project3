f92023d2b6fc9af25de8265d3c8212f16ecfc307
JordanLarson <jdlarson1234@gmail.com>
2020-07-17 12:23:46 -0700

    user schema complete

---
+++ models/users.js
@@ -0,0 +1,18 @@
+const mongoose = require("../db/connection");
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  image: { type: File, required: true },
+  location: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, required: true },
+  antibodies: { type: Boolean, default: true },
+  likesYou: { type: Boolean, default: true },
+  isFavorite: { type: Boolean, default: false },
+  hobbiesBefore: { type: String, required: true },
+  hobbiesAfter: { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
