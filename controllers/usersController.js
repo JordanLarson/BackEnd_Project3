438bf84241dfbbe99a1b92062c58d3c30e3bdc55
JordanLarson <jdlarson1234@gmail.com>
2020-07-19 10:56:45 -0700

    ugh

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
-const seedData = require("../db/seedUsers");
 
 router.get("/", (req, res) => res.send("This is root, yeww"));
 
@@ -14,5 +13,16 @@ router.get("/users", async (req, res) => {
     return res.status(500).send(error.message);
   }
 });
-
+router.get("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (user) {
+      return res.status(200).json({ user });
+    }
+    return res.status(404).send("User with the specified ID does not exist");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
 module.exports = router;
--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 
-// let MONGODB_URI = 'mongodb://127.0.0.1:27017/itemsDatabase'
 let MONGODB_URI = "";
 
 if (process.env.NODE_ENV === "production") {
