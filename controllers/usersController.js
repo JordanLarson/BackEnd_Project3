72016b646dc13945b5cad80c73d72800022057c1
Anthony Hart <CheetahGuy@Anthonys-MacBook-Pro-3.local>
2020-07-19 21:08:01 -0400

    added routes

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -15,4 +15,29 @@ router.get("/users", async (req, res) => {
   }
 });
 
+router.delete('/users/:id', async (req, res) => {
+  try {
+      const { id } = req.params;
+      const deleted = await User.findByIdAndDelete(id)
+      if (deleted) {
+          return res.status(200).send("User deleted");
+      }
+      throw new Error("Item not found");
+  } catch (error) {
+      return res.status(500).send(error.message);
+  }
+})
+
+router.post("/users", (req, res) => {
+  User.create(req.body, (error, newUser) => {
+    if (error) console.log(error);
+    else res.send(newUser);
+  });
+  const newUser = req.body;
+    User.push(newUser);
+    res.send(newUser);
+});
+
+
+
 module.exports = router;
